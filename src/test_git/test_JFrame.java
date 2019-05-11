fe4b032c4cf55d44cade47a9f5af273da8da6055
beo <beo@DESKTOP>
2019-05-11 13:09:46 +0900

    3つのボタン追加

--- src/test_git/test_JFrame.java
+++ src/test_git/test_JFrame.java
@@ -1,13 +1,29 @@
 package test_git;
 
+import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 class MyFrame extends JFrame{
+	JButton b1,b2,b3;
 	public MyFrame() {
+		Container contentPane = getContentPane();
+		setTitle("My Window Application");
 		setSize(300,200);
+		contentPane.setLayout(new FlowLayout());
+
+		b1 = new JButton("Button1");
+		b2 = new JButton("Button2");
+		b3 = new JButton("Button3");
+
+		contentPane.add(b1);
+		contentPane.add(b2);
+		contentPane.add(b3);
+
 		addWindowListener(new MyWindowAdapter());
 	}
 }
