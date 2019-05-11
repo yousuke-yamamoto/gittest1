d4be04d86d1dd6048d75b6f31e00b0349b7aac4c
beo <beo@DESKTOP>
2019-05-11 13:29:24 +0900

    ボタンアクションの追加

--- src/test_git/test_JFrame.java
+++ src/test_git/test_JFrame.java
@@ -2,31 +2,47 @@ package test_git;
 
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-class MyFrame extends JFrame{
+class MyFrame extends JFrame implements ActionListener{
 	JButton b1,b2,b3;
 	public MyFrame() {
 		Container contentPane = getContentPane();
 		setTitle("My Window Application");
 		setSize(300,200);
+		addWindowListener(new MyWindowAdapter());
 		contentPane.setLayout(new FlowLayout());
 
 		b1 = new JButton("Button1");
 		b2 = new JButton("Button2");
 		b3 = new JButton("Button3");
 
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
+
 		contentPane.add(b1);
 		contentPane.add(b2);
 		contentPane.add(b3);
-
-		addWindowListener(new MyWindowAdapter());
 	}
+
+	public void actionPerformed(ActionEvent ae){
+		if(ae.getSource() == b1)
+			System.out.println("Button1がクリック");
+		else if(ae.getSource() == b2)
+			System.out.println("Button2がクリック");
+		else
+			System.out.println("Button3がクリック");
+	}
+
 }
+
 
 class MyWindowAdapter extends WindowAdapter{
 	public void windowClosing(WindowEvent e) {
