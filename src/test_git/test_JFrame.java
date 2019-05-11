421634f56edb31dfb8e6b0d5a4e73290236bc147
beo <beo@DESKTOP>
2019-05-11 12:58:23 +0900

    閉じる機能の追加

--- src/test_git/test_JFrame.java
+++ src/test_git/test_JFrame.java
@@ -1,12 +1,26 @@
 package test_git;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 
+class MyFrame extends JFrame{
+	public MyFrame() {
+		setSize(300,200);
+		addWindowListener(new MyWindowAdapter());
+	}
+}
+
+class MyWindowAdapter extends WindowAdapter{
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+	}
+}
 public class test_JFrame {
 
 	public static void main(String[] args) {
-		JFrame f = new JFrame();
-		f.setSize(300,200);
+		MyFrame f = new MyFrame();
 		f.setVisible(true);
 	}
 
